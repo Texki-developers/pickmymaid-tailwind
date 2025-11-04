1670d4f4c09093837e40409d4c6552a748afbe50
Muhsin Neyyathur <70310447+MUHSIN22@users.noreply.github.com>
2025-11-04 21:48:45 +0530

    gtag fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,19 +88,30 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1344507103340371&ev=PageView&noscript=1"
           />
         </noscript>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7WGQXM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
       <>
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=AW-11255924314`}
-        />
-        <Script id="gtag-init" strategy="lazyOnload">
+       {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'AW-11255924314');
-  `}
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M7WGQXM');
+          `}
         </Script>
         <Script id="clarity-script" strategy="lazyOnload">
           {`
